62520937171427166f8f245a2423f6609ca704ed
Edith-Agoye Daniel Olamide <mdon85329@gmail.com>
2025-07-19 13:09:57 +0100

    ...

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ let butt = document.getElementById("bott");
 let mained = document.getElementById("mained")
 let view = document.getElementById("view")
 let views = document.getElementById("views")
-butt.addEventListener("click", function(){
-    view.classList.toggle("hidden")
-    views.classList.toggle("hidden")
+let email = document.getElementById("email")
+butt.addEventListener("click", function(e){
+    e.preventDefault();
+    let emails = email.value.trim();
+    if (emails === "" || !email.includes("@") || !email.includes(".com")) {
+        alert("cewio")
+        return
+    }else{
+    view.classList.add("hidden")
+    views.classList.remove("hidden")
+
+
+    }
+
 
 })
