daa3cdb490d131fcb923adc6f83a6d3bb2d36365
Edith-Agoye Daniel Olamide <mdon85329@gmail.com>
2025-07-19 12:44:55 +0100

    ...

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 let butt = document.getElementById("bott");
 let mained = document.getElementById("mained")
-let viewa = document.getElementById("view")
-let viewb = document.getElementById("views")
-butt.addEventListener("click", function (e) {
-    e.preventDefault()
+let view = document.getElementById("view")
+let views = document.getElementById("views")
+butt.addEventListener("click", function(){
+    view.classList.toggle("hidden")
+    views.classList.toggle("hidden")
 
-    let emai = document.getElementById("emai").value.trim();
-    const warn = document.getElementById("warn")
-    if (emai === "") {
-        warn.classList.remove("hidden")
-        return
-    }
-    else{
-        warn.classList.add("hidden")
-    
-    viewa.classList.add("hidden")
-    viewb.classList.remove("hidden")
-    }
 })
